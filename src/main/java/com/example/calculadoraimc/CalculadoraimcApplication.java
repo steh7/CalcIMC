bcb63dc123cc0a56ba96fbf5db7dbb0a3a868924
steh7 <stephanychryscv@gmail.com>
2025-05-30 20:08:43 -0300

    Calculadora de IMC

---
+++ src/main/java/com/example/calculadoraimc/CalculadoraimcApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculadoraimc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraimcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraimcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/calculadoraimc/IMCController.java
@@ -0,0 +1,30 @@
+package com.example.calculadoraimc;
+ 
+import org.springframework.web.bind.annotation.*;
+ 
+@RestController
+@RequestMapping("/imc")
+public class IMCController {
+ 
+    @GetMapping
+    public IMCResponse calcularIMC(@RequestParam double peso, @RequestParam double altura) {
+        double imc = peso / (altura * altura);
+        String classificacao;
+ 
+        if (imc < 18.5) {
+            classificacao = "Abaixo do peso";
+        } else if (imc < 24.9) {
+            classificacao = "Peso normal";
+        } else if (imc < 29.9) {
+            classificacao = "Sobrepeso";
+        } else if (imc < 34.9) {
+            classificacao = "Obesidade grau I";
+        } else if (imc < 39.9) {
+            classificacao = "Obesidade grau II";
+        } else {
+            classificacao = "Obesidade grau III ou mórbida";
+        }
+ 
+        return new IMCResponse(imc, classificacao);
+    }
+}
